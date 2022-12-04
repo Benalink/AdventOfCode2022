ac22a96e2f2012bfb437a282a01db39f10b13fb6
Ben Alink <benalink@live.co.uk>
2022-12-04 10:53:29 +0000

    Solve Day 4

---
+++ AdventOfCode/Day04.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode;
+
+public sealed class Day04 : BaseDay
+{
+    private readonly string input;
+
+    public Day04()
+    {
+        this.input = File.ReadAllText(this.InputFilePath);
+    }
+
+    private IEnumerable<(int[] A, int[] B)> ParseInput() =>
+        this.input.Split('\n').Select(line =>
+        {
+            string[] pairSplit = line.Split(',');
+            IEnumerable<int>[] ranges = pairSplit.Select(range =>
+            {
+                int[] boundSplit = range.Split('-').Select(int.Parse).ToArray();
+                return Enumerable.Range(boundSplit[0], boundSplit[1] - boundSplit[0] + 1);
+            }).ToArray();
+
+            return (ranges[0].ToArray(), ranges[1].ToArray());
+        });
+
+    public override ValueTask<string> Solve_1()
+    {
+        int fullyContainedCount = ParseInput().Count(pair =>
+        {
+            int minLength = Math.Min(pair.A.Length, pair.B.Length);
+            return pair.A.Intersect(pair.B).Count() == minLength;
+        });
+
+        return new ValueTask<string>(fullyContainedCount.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int overlapsCount = ParseInput().Count(pair => pair.A.Intersect(pair.B).Any());
+        return new ValueTask<string>(overlapsCount.ToString());
+    }
+}
+
